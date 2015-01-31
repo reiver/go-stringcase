40cc83ec92e266af1eda0b73fd6d1187aa7db24a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-01-31 00:55:23 -0800

    added some docs.

---
+++ doc.go
@@ -0,0 +1,43 @@
+/*
+Package stringcase makes it so you can convert strings to different casing styles:
+
+* lower case
+* UPPER CASE
+* Title Case
+* camelCase
+* PascalCase
+* snake_case
+* CONST_CASE
+* property-case
+* Header-Case
+
+Samples
+	s := stringcase.ToLowerCase("Hello world")
+	// hello world
+
+	s := stringcase.ToUpperCase("Hello world")
+	// HELLO WORLD
+
+	s := stringcase.ToTitleCase("Hello world")
+	// Hello World
+
+	s := stringcase.ToCamelCase("Hello world")
+	// helloWorld
+
+	s := stringcase.ToPascalCase("Hello world")
+	// HelloWorld
+
+	s := stringcase.ToSnakeCase("Hello world")
+	// hello_world
+
+	s := stringcase.ToConstCase("Hello world")
+	// HELLO_WORLD
+
+	s := stringcase.ToPropertyCase("Hello world")
+	// hello-world
+
+	s := stringcase.ToHeaderCase("Hello world")
+	// Hello-World
+
+*/
+package stringcase
