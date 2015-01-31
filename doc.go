32198f18e38010f91410efa241d0703c2f354c6e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-01-31 00:57:47 -0800

    tweaking style of docs

--- doc.go
+++ doc.go
@@ -22,28 +22,28 @@ Package stringcase makes it so you can convert strings to different casing style
 Samples
 	s := stringcase.ToLowerCase("Hello world")
 	// hello world
-
+.
 	s := stringcase.ToUpperCase("Hello world")
 	// HELLO WORLD
-
+.
 	s := stringcase.ToTitleCase("Hello world")
 	// Hello World
-
+,
 	s := stringcase.ToCamelCase("Hello world")
 	// helloWorld
-
+.
 	s := stringcase.ToPascalCase("Hello world")
 	// HelloWorld
-
+.
 	s := stringcase.ToSnakeCase("Hello world")
 	// hello_world
-
+.
 	s := stringcase.ToConstCase("Hello world")
 	// HELLO_WORLD
-
+.
 	s := stringcase.ToPropertyCase("Hello world")
 	// hello-world
-
+.
 	s := stringcase.ToHeaderCase("Hello world")
 	// Hello-World
 
